c8b642b2b1c08147f67acb011276550a2cae2fbc
erfanHP390 <erfanhossini768@yahoo.com>
2024-11-26 21:44:58 +0330

    create compo free-plan

---
+++ src/components/FreePlan/FreePlan.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function FreePlan () {
+  return (
+    <>
+      <div class='freeplan align-items-center justify-content-center flex-column p-5'  >
+        <div class='d-flex justify-content-center align-items-center flex-column'>
+          <h1 class='white-text'>دوره آزمایشی رایگان ۳۰ روزه را شروع کنید.</h1>
+          <p class='white-text text-wrap text'>
+            این یک واقعیت ثابت طولانی مدت است که خواننده با محتوای خواندنی پرت
+            می شود.
+          </p>
+          <a
+            href='#'
+            class='free-plan-btn redbtn blue-bg  pt-2 pb-2 p-5'
+          >
+            شروع کنید
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
